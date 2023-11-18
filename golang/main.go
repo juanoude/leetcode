20ade1e4111ad71acd879af9c7170f054d070623
Juan Ananda <juanoude@hotmail.com>
2023-11-18 19:12:21 -0300

    golang - merge two sorted lists

--- golang/main.go
+++ golang/main.go
@@ -6,7 +6,27 @@ import (
 )
 
 func main() {
-	arg := "()"
-	result := algorithms.IsValidParentheses(arg)
+	arg4 := algorithms.ListNode{
+		Val:  4,
+		Next: nil,
+	}
+
+	arg3 := algorithms.ListNode{
+		Val:  3,
+		Next: &arg4,
+	}
+
+	arg2 := algorithms.ListNode{
+		Val:  2,
+		Next: nil,
+	}
+
+	arg1 := algorithms.ListNode{
+		Val:  1,
+		Next: &arg2,
+	}
+
+	result := algorithms.MergeTwoLists(&arg1, &arg3)
 	fmt.Println(result)
+	fmt.Printf("%+v", result)
 }

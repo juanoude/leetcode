4375e50c146f9fe9d09ebedd11e2ae2e24d4d30b
Juan Ananda <juanoude@gmail.com>
2024-01-21 20:08:30 -0300

    changes: - golang - binary tree maximum depth

--- golang/main.go
+++ golang/main.go
@@ -6,8 +6,25 @@ import (
 )
 
 func main() {
-	arg := []int{-1, 1}
-	result := algorithms.ThreeSum(arg)
+	arg2 := algorithms.TreeNode{
+		Val:   10,
+		Left:  nil,
+		Right: nil,
+	}
+
+	arg3 := algorithms.TreeNode{
+		Val:   20,
+		Left:  nil,
+		Right: nil,
+	}
+
+	arg := algorithms.TreeNode{
+		Val:   15,
+		Left:  &arg2,
+		Right: &arg3,
+	}
+
+	result := algorithms.MaxDepth(&arg)
 	// fmt.Println(arg)
 	fmt.Println(result)
 }

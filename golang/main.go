278723fbba1dc4bc0dacd84284be897d1285f262
Juan Ananda <juanoude@gmail.com>
2024-05-04 13:06:43 -0300

    changes: - add majority element algo

--- golang/main.go
+++ golang/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"leetcode_golang/algorithms"
 )
 
 func main() {
-	arg := []int{-1, 0, 0, 3, 3, 3, 0, 0, 0}
-	arg2 := []int{1, 2, 2}
-
-	algorithms.Merge(arg, 6, arg2, 3)
+	arg := []int{-1, 0, 0, 3, 3, 3, 3, 3}
+	result := algorithms.MajorityElement(arg)
+	fmt.Println(result)
 }
